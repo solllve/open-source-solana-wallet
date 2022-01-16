a3612e62d45c3e4691c6afde952aabca5d0cc155
solllve <caseyhald@Caseys-MacBook-Pro.local>
2022-01-16 08:36:41 -0800

    created connection

--- App.js
+++ App.js
@@ -2,13 +2,45 @@ import { StyleSheet, Text, View } from 'react-native';
 import {createConnection} from './services';
 import store from './store';
 import { Provider } from 'react-redux'
-export default function App() {
+import { Button } from 'react-native-elements';
+import { Input, Icon } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Authenticate"
+      onPress={() => {
+        navigation.navigate('Profile', { name: 'Casey' })
+      }}
+    />
+  );
+};
+const ProfileScreen = ({ navigation, route }) => {
+  createConnection();
+  return (
+    <Text>This is {route.params.name}'s profile</Text>
+  )
+  
+};
+
+export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Welcome' }}
+          />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      
     </Provider>
   );
 }
--- services/index.js
+++ services/index.js
@@ -2,7 +2,8 @@
 import * as solanaWeb3 from '@solana/web3.js';
 
 const createConnection = () => {
-    return new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("devnet"));
+    //return new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("devnet"));
+    alert(solanaWeb3)
 };
 
 export {createConnection}
