7040945ef93c7f7d16e79d569e5ac27b23b5e580
solllve <caseyhald@Caseys-MacBook-Pro.local>
2022-01-15 21:39:04 -0800

    installed solana api

---
+++ services/index.js
@@ -0,0 +1,8 @@
+
+import * as solanaWeb3 from '@solana/web3.js';
+
+const createConnection = () => {
+    return new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("devnet"));
+};
+
+export {createConnection}
